1d65473d7b08c794a83dedc16303830e1fc7a478
BitisGabonica <0xBitisGabonica@protonmail.com>
2023-06-04 17:37:54 +0200

    add test

--- bug.c
+++ bug.c
@@ -1,18 +1,10 @@
 // Define main function:
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-
 int x = 1;
 
 int main(int argc, char *argv[]) {
     if (5 % (3 * x) + 2 != 4) {
-        printf("Hello, world!\n");
-    } else {
-        printf("Goodbye, world!\n");
+       __builtin_abort();
     }
     return 0;
 }
