1ac8daf554b337db1f00e34a64042e98437350a1
Holmness <xpjakkexp@gmail.com>
2023-06-06 01:00:58 +0200

    Added actual bug

--- bug.c
+++ bug.c
@@ -1,10 +1,22 @@
-// Define main function:
+#include <stdio.h>
 
-int x = 1;
+#define DEBUG_NOPRINT "\0NOPRINT"
 
-int main(int argc, char *argv[]) {
-    if (5 % (3 * x) + 2 != 4) {
-       __builtin_abort();
-    }
-    return 0;
+#define NO_DEBUG_INFO(dbg_lvl) (__builtin_memcmp(dbg_lvl, DEBUG_NOPRINT, sizeof(DEBUG_NOPRINT)))
+
+#define debug_continue(condition, dbg_lvl, str, ...) {                         \
+    int no_info = NO_DEBUG_INFO(dbg_lvl);                                      \
+    if (condition && (no_info != 0)) {                                         \
+        printf("Didn't work, try again!\n",no_info);                           \
+        continue;                                                              \
+    }                                                                          \
+}
+
+int main() {
+   char DEBUG_PRINT[9] = "";
+   for (int i = 0; i < 10; i++) {
+      debug_continue((0 != 1), DEBUG_PRINT, "No user match, continuing to search\n");
+      printf("Hacked\n");
+   }
+   return 0;
 }
